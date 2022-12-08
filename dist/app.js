c715298ee8695342fa9012f69ecfb7b3f8bf4dbc
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-12-08 12:36:24 +0530

    Basic express server setup  done with types ,used dotenv env variables and implemented middleware for handling errors

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log("running...");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importDefault(require("http-errors"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const PORT = Number(process.env.PORT) || 3000;
+const app = (0, express_1.default)();
+app.get("/test", (req, res, next) => {
+    res.send("Hello from server..");
+});
+//This middleware throws error if end point/url not found
+app.use((req, res, next) => {
+    next(new http_errors_1.default.NotFound());
+});
+//This middleware handles appilcation error
+const errorHandler = (error, req, res, next) => {
+    res.status(error.status || 500);
+    res.send({ status: error.status || 500, message: error.message });
+};
+app.use(errorHandler);
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
-console.log("running...")
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  Application,
+  ErrorRequestHandler,
+} from "express";
+import createHttpError from "http-errors";
+import { config } from "dotenv";
+
+config();
+
+const PORT: Number = Number(process.env.PORT) || 3000;
+const app: Application = express();
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello from server..");
+});
+
+//This middleware throws error if end point/url not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(new createHttpError.NotFound());
+});
+
+//This middleware handles appilcation error
+const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({ status: error.status || 500, message: error.message });
+};
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
