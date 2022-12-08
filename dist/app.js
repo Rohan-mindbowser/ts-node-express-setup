fc37fad45bf9fb8476ecfe29f14d67d9e0cc4497
Rohan Kadam <rohan.kadam@mindbowser.com>
2022-12-08 15:07:12 +0530

    Implemented morgan logger for logging all the requests

--- dist/app.js
+++ dist/app.js
@@ -6,17 +6,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const http_errors_1 = __importDefault(require("http-errors"));
 const dotenv_1 = require("dotenv");
+var morgan = require("morgan");
+var fs = require("fs");
+var path = require("path");
 (0, dotenv_1.config)();
 const PORT = Number(process.env.PORT) || 3000;
 const app = (0, express_1.default)();
+// //This will log all the requests
+// app.use(morgan("common"));
+// log only 4xx and 5xx responses to console
+app.use(morgan("common", {
+    skip: function (req, res) {
+        return res.statusCode < 400;
+    },
+}));
+// log all requests to access.log
+app.use(morgan("common", {
+    stream: fs.createWriteStream(path.join(__dirname, "logs/error.log"), {
+        flags: "a",
+    }),
+}));
 app.get("/test", (req, res, next) => {
     res.send("Hello from server..");
 });
 //This middleware throws error if end point/url not found
 app.use((req, res, next) => {
-    next(new http_errors_1.default.NotFound());
+    next(new http_errors_1.default.NotFound("URL not found"));
 });
-//This middleware handles appilcation error
+//This middleware handles application error
 const errorHandler = (error, req, res, next) => {
     res.status(error.status || 500);
     res.send({ status: error.status || 500, message: error.message });
--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,26 @@ import express, {
 } from "express";
 import createHttpError from "http-errors";
 import { config } from "dotenv";
+var morgan = require("morgan");
+var fs = require("fs");
+var path = require("path");
 
 config();
 
 const PORT: Number = Number(process.env.PORT) || 3000;
 const app: Application = express();
+
+//This will log all the requests
+app.use(morgan("common"));
+
+// log all requests to access.log
+app.use(
+  morgan("common", {
+    stream: fs.createWriteStream(path.join(__dirname, "logs/access.log"), {
+      flags: "a",
+    }),
+  })
+);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello from server..");
@@ -19,10 +34,10 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
 
 //This middleware throws error if end point/url not found
 app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new createHttpError.NotFound());
+  next(new createHttpError.NotFound("URL not found"));
 });
 
-//This middleware handles appilcation error
+//This middleware handles application error
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   res.status(error.status || 500);
   res.send({ status: error.status || 500, message: error.message });
